a88d32c3d4db39c6efdf6447563d2ad791c6eaaa
LaidBenglia <73167661+LaidBengli@users.noreply.github.com>
2023-09-08 15:54:25 -0700

    Done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+// Function to write thank you messages for an array of names
+function writeCards(names, event) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  const names = ["Charlie", "Samip", "Ali"];
+  const event = "birthday";
+  const messages = writeCards(names, event);
+  
+  console.log(messages);
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  const startNumber = 10;
+  countDown(startNumber);
